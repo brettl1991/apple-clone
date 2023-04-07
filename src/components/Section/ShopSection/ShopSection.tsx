ab403855b98265ba8eaa5906aeb54e48cd529bb2
brettl1991 <brettl1991@gmail.com>
2023-04-07 15:27:07 +0100

    feat: style shopsection, add sup element

--- src/components/Section/ShopSection/ShopSection.tsx
+++ src/components/Section/ShopSection/ShopSection.tsx
@@ -9,18 +9,60 @@ const ShopSection = ({
   urlTitle,
 }: shopSectionType) => {
   return (
-    <span>
-      {title}
-      <a href={url}>
-        {urlTitle}
-        <Img src={svgPath} alt="right-chevron" />
-      </a>
-    </span>
+    <Container>
+      <Text>
+        {title}{" "}
+        <sup>
+          <a href="#">1</a>
+        </sup>
+        &nbsp;&nbsp;&nbsp;
+        <a href={url}>
+          {urlTitle}
+          <span>
+            <Img src={svgPath} alt="right-chevron" />
+          </span>
+        </a>
+      </Text>
+    </Container>
   );
 };
 
 export default ShopSection;
 
+const Container = styled.section`
+  text-align: center;
+`;
+
+const Text = styled.span`
+  font-size: 14px;
+  display: inline-block;
+  padding: 12px 0;
+
+  sup {
+    position: relative;
+    font-size: 0.6em;
+    vertical-align: baseline;
+
+    a {
+      position: absolute;
+      color: black;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: #06c;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
 const Img = styled.img`
+  padding-left: 0.2em;
+  color: #06c;
   width: 12px;
+
+  letter-spacing: inherit;
 `;
